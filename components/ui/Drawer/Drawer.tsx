5657bfe60fc2b33a25f333c75e40cf55fae86ebb
Ramis <71335046+KhasanshinRamis@users.noreply.github.com>
2023-08-28 12:48:01 +0300

    frame-motion

--- components/ui/Drawer/Drawer.tsx
+++ components/ui/Drawer/Drawer.tsx
@@ -29,42 +29,42 @@ export const Drawer = ({ ...props }: DrawerProps): JSX.Element => {
 
 	return (
 		<div className={styles.drawer} {...props}>
-		{whichElement === 'add' 
-		? 
-			<div className={styles.drawerAddTask}>
-				<div className={styles.drawerHeader}>
-					<span className={styles.drawerHeaderTitle}>Create new task</span>
-					<ButtonIcon appearance='close' onClick={() => setIsOpened(false)} />
-				</div>
+			{whichElement === 'add' 
+			? 
+				<div className={styles.drawerAddTask}>
+					<div className={styles.drawerHeader}>
+						<span className={styles.drawerHeaderTitle}>Create new task</span>
+						<ButtonIcon appearance='close' onClick={() => setIsOpened(false)} />
+					</div>
 
-				<div className={styles.drawerBody}>
-					<Input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
-				</div>
+					<div className={styles.drawerBody}>
+						<Input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+					</div>
 
-				<div className={styles.drawerFooter}>
-					<Button appearance='primary' onClick={() => setIsOpened(false)}>Cancel</Button>
-					<Button appearance='add' onClick={onAdd}>Save</Button>
+					<div className={styles.drawerFooter}>
+						<Button appearance='primary' onClick={() => setIsOpened(false)}>Cancel</Button>
+						<Button appearance='add' onClick={onAdd}>Save</Button>
+					</div>
 				</div>
-			</div>
-		: 
-			<div className={styles.drawerChangeTask}>
+			: 
+				<div className={styles.drawerChangeTask}>
 
-				<div className={styles.drawerHeader}>
-					<span className={styles.drawerHeaderTitle}>Change title task</span>
-					<ButtonIcon appearance='close' onClick={() => setIsOpened(false)} />
+					<div className={styles.drawerHeader}>
+						<span className={styles.drawerHeaderTitle}>Change title task</span>
+						<ButtonIcon appearance='close' onClick={() => setIsOpened(false)} />
+					</div>
+
+					<div className={styles.drawerBody}>
+						<Input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+					</div>
+
+					<div className={styles.drawerFooter}>
+						<Button appearance='primary' onClick={() => setIsOpened(false)}>Cancel</Button>
+						<Button appearance='add' onClick={onUpdate}>Save</Button>
+					</div>
+
 				</div>
-
-				<div className={styles.drawerBody}>
-					<Input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
-				</div>
-
-				<div className={styles.drawerFooter}>
-					<Button appearance='primary' onClick={() => setIsOpened(false)}>Cancel</Button>
-					<Button appearance='add' onClick={onUpdate}>Save</Button>
-				</div>
-
-			</div>
-		}
+			}
 		</div>
 	);
 };
--- components/widget/CreateTask/CreateTask.tsx
+++ components/widget/CreateTask/CreateTask.tsx
@@ -4,6 +4,7 @@ import { CreateTaskProps } from './CreateTask.props';
 import styles from './CreateTask.module.scss';
 import { Button, Drawer, BackDrop } from '@/components/ui';
 import { useDrawerStore } from '@/store/useDrawerStore';
+import { motion } from 'framer-motion';
 
 
 export const CreateTask = ({ ...props }: CreateTaskProps): JSX.Element => {
@@ -11,6 +12,38 @@ export const CreateTask = ({ ...props }: CreateTaskProps): JSX.Element => {
 
 	const [ isOpened, setIsOpened, setWhichElement] = useDrawerStore((state) => [state.isOpened, state.setIsOpened, state.setWhichElement]);
 
+	const variants = {
+		visible: {
+			position: 'absolute',
+			width: '100%',
+			height: '100%',
+			x: '0%',
+			top: 0,
+			right:0,
+			left: 0,
+			bottom: 0,
+			opacity: 1,
+			transition: {
+			duration: 0.3,
+			ease: 'easeInOut',
+			},
+		},
+		hidden: {
+			position: 'absolute',
+			width: '0%',
+			height: '0%',
+			top: 0,
+			right:0,
+			left: 0,
+			bottom: 0,
+			x: '100%',
+			opacity: 1,
+			transition: {
+			duration: 0.3,
+			ease: 'easeInOut',
+			},
+		},
+	};
 
 	return (
 		<div
@@ -27,7 +60,13 @@ export const CreateTask = ({ ...props }: CreateTaskProps): JSX.Element => {
 			>
 				Add new task
 			</Button>
-			{isOpened ? <> <BackDrop /> <Drawer /> </> : ''}
+			<motion.div
+				variants={variants}
+				initial={isOpened ? 'visible' : 'hidden'}
+				animate={isOpened ? 'visible' : 'hidden'}
+			>
+				{isOpened ? <> <BackDrop /> <Drawer /> </> : ''}
+			</motion.div>
 		</div>
 	);
 };
